19eb261464f0f9d97a3479bf01018b8dfcd06681
Allen <allenvs@mail.ru>
2022-08-25 15:49:40 +0300

    time.Until - sweet

--- sprint1/time/duration/duration.go
+++ sprint1/time/duration/duration.go
@@ -8,7 +8,9 @@ import (
 
 func main() {
 	// скопируйте блок себе в IDE и допишите код
+
 	birthday := time.Date(2093, time.November, 26, 0, 0, 0, 0, time.Local)
-	days := birthday.Sub(time.Now()).Truncate(time.Hour*24).Hours() / 24
+	duration := time.Until(birthday)
+	days := int(duration.Hours() / 24)
 	fmt.Println(days)
 }
