6774b4ea6e4cea8b153c0a30fd3180d6c28fe5ca
Allen <allenvs@mail.ru>
2022-08-25 15:45:20 +0300

    duration solved?

--- sprint1/time/duration/duration.go
+++ sprint1/time/duration/duration.go
@@ -1,8 +1,14 @@
+// Андрей родился 26 ноября 1993 года. Посчитайте количество дней до его 100-летия — относительно сегодняшнего дня.
 package main
+
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	// скопируйте блок себе в IDE и допишите код
-	// birthday := time.Date(... , time.Local)
-	// days := ...
-	// fmt.Println(days)
+	birthday := time.Date(2093, time.November, 26, 0, 0, 0, 0, time.Local)
+	days := birthday.Sub(time.Now()).Truncate(time.Hour*24).Hours() / 24
+	fmt.Println(days)
 }
