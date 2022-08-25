dea5cc0a1970fae9b82e75c33d4bd2d10dd73685
Allen <allenvs@mail.ru>
2022-08-25 14:19:23 +0300

    solved task 5 time compare

--- sprint1/time/compare/task5camparetime.go
+++ sprint1/time/compare/task5camparetime.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	currentTimeStr := "2021-09-19T15:59:41+03:00"
 	// скопируйте блок себе в IDE и допишите код
-	layout := "..."
+	layout := time.RFC3339
 	currentTime, err := time.Parse(layout, currentTimeStr)
 	if err != nil {
 		log.Fatal(err)
