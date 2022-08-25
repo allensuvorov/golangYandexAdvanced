ea6e1c40c254d343e02c72a1b54e33e7316930a4
Allen <allenvs@mail.ru>
2022-08-25 13:47:03 +0300

    solved time date parcing

--- sprint1/time/task4parcingtime.go
+++ sprint1/time/task4parcingtime.go
@@ -8,8 +8,11 @@ import (
 
 func main() {
 	currentTimeStr := "2021-09-19T15:59:41+03:00"
-	// скопируйте блок себе в IDE и допишите код
-	layout := "..."
+
+	// we can use this
+	layout := "2006-01-02T15:04:05-07:00"
+	// or predefined layouts
+	// layout := time.RFC3339 //	RFC3339     = "2006-01-02T15:04:05Z07:00"
 	currentTime, err := time.Parse(layout, currentTimeStr)
 	if err != nil {
 		log.Fatal(err)
