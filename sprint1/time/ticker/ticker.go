f53980a7a0e313cf2dd22f408d005c903e32d835
Allen <allenvs@mail.ru>
2022-08-25 18:41:26 +0300

    ticker ticks 10 times, just need to get clean seconds only

--- sprint1/time/ticker/ticker.go
+++ sprint1/time/ticker/ticker.go
@@ -1,5 +1,19 @@
+// Задание 8
+// Используя Ticker, напишите программу, которая каждые 2 секунды 10 раз подряд выводит разницу в секундах между текущим временем и временем запуска программы. Выводить нужно только целую часть секунд.
+
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
-	// скопируйте блок себе в IDE и допишите код
+	start := time.Now()
+	ticker := time.NewTicker(2 * time.Second) // created a ticker
+	for i := 0; i < 10; i++ {
+		t := <-ticker.C // wait for ticker
+		fmt.Println(t.Sub(start).Seconds())
+	}
+	ticker.Stop()
 }
