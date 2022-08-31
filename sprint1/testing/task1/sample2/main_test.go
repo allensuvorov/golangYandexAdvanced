2597585142984d974df99cae7eddb0c5dbc8929c
Allen <allenvs@mail.ru>
2022-08-31 16:31:53 +0300

    testing, sample2 - done

--- sprint1/testing/task1/sample2/main_test.go
+++ sprint1/testing/task1/sample2/main_test.go
@@ -1,44 +1,43 @@
-// для функции Abs проверьте тесты на -3, 3, -2.000001, -0.000000003 и другие значения.
 package main
 
 import "testing"
 
-func TestAbs(t *testing.T) {
+func TestFullName(t *testing.T) {
 	tests := []struct { // добавился слайс тестов
 		name  string
-		value float64
-		want  float64
+		value User
+		want  string
 	}{
 		{
-			name:  "negative int", // описывается каждый тест
-			value: -3,             // значения, которые будет принимать функция
-			want:  3,              // ожидаемое значение
+			name:  "empty first name", // описывается каждый тест
+			value: User{"", "Popov"},  // значения, которые будет принимать функция
+			want:  "Popov",            // ожидаемое значение
 		},
 		{
-			name:  "positive int",
-			value: 3,
-			want:  3,
+			name:  "empty last name",
+			value: User{"Sasha", ""},
+			want:  "Sasha",
 		},
 		{
-			name:  "negative float",
-			value: -2.000001,
-			want:  2.000001,
+			name:  "all caps",
+			value: User{"SASHA", "IVANOV"},
+			want:  "SASHA IVANOV",
 		},
 		{
-			name:  "negative float with leading zero",
-			value: -0.000000003,
-			want:  0.000000003,
+			name:  "with middle name",
+			value: User{"Ivan Ivanovich", "Ivanov"},
+			want:  "Ivan Ivanovich Ivanov",
 		},
 		{
-			name:  "negative big int - 100billion",
-			value: -100_000_000_000,
-			want:  100_000_000_000,
+			name:  "all lower case",
+			value: User{"ivan", "ivanov"},
+			want:  "ivan ivanov",
 		},
 	}
 	for _, tt := range tests { // цикл по всем тестам
 		t.Run(tt.name, func(t *testing.T) {
-			if abs := Abs(tt.value); abs != tt.want {
-				t.Errorf("Add() = %v, want %v", abs, tt.want)
+			if fn := User.FullName(tt.value); fn != tt.want {
+				t.Errorf("Add() = %v, want %v", fn, tt.want)
 			}
 		})
 	}
