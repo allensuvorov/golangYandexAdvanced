0501112cdb328e321e43e0409a82240ea0078eae
Allen <allenvs@mail.ru>
2022-09-18 13:13:05 +0300

    sprint2/encoding/stdtools/task1 done

--- sprint2/encoding/standardtools/task1/main.go
+++ sprint2/encoding/standardtools/task1/main.go
@@ -1,29 +1,32 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "log"
+	"encoding/json"
+	"fmt"
+	"log"
 )
 
 type Data struct {
-    // определите здесь нужные поля
+	// определите здесь нужные поля
+	ID      int    `json:"-"`
+	Name    string `json:"name,omitempty"`
+	Company string `json:"company,omitempty"`
 }
 
 func main() {
-    foo := []Data{
-        {
-            ID:   10,
-            Name: "John Doe",
-        },
-        {
-            Name:    "Вася",
-            Company: "Яндекс",
-        },
-    }
-    out, err := json.Marshal(foo)
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Println(string(out))
+	foo := []Data{
+		{
+			ID:   10,
+			Name: "John Doe",
+		},
+		{
+			Name:    "Вася",
+			Company: "Яндекс",
+		},
+	}
+	out, err := json.Marshal(foo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(out))
 }
