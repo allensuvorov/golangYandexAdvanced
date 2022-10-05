5cfbafcd139d94cf1ba25448ce9bd017910912b7
Allen <allenvs@mail.ru>
2022-10-05 22:11:16 +0600

    copied solution

--- sprint2/flag/task12/main.go
+++ sprint2/flag/task12/main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 )
 
 var version = "0.0.1"
 
 func main() {
 	// допишите код
+
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Version of %s\n%v\nUsage :\n", os.Args[0], version)
+		flag.PrintDefaults()
+	}
+
 	flag.Parse()
 }
