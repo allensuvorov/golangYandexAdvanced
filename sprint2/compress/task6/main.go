bdb7ff97209c4625471a06f4cf150c2dba50c611
Allen <allenvs@mail.ru>
2022-10-10 17:56:59 +0600

    compress task6 - my first try

--- sprint2/compress/task6/main.go
+++ sprint2/compress/task6/main.go
@@ -1,20 +1,46 @@
+package main
+
+import (
+	"compress/gzip"
+	"fmt"
+	"io"
+	"net/http"
+)
+
 // LengthHandle возвращает размер распакованных данных.
 func LengthHandle(w http.ResponseWriter, r *http.Request) {
 	// создаём *gzip.Reader, который будет читать тело запроса
 	// и распаковывать его
-	gz, err := gzip.NewReader(r.Body)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	// не забывайте потом закрыть *gzip.Reader
-	defer gz.Close()
+	if _, ok := r.Header["Content-Encoding"]; ok {
 
-	// при чтении вернётся распакованный слайс байт
-	body, err := io.ReadAll(gz)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		gz, err := gzip.NewReader(r.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		// не забывайте потом закрыть *gzip.Reader
+		defer gz.Close()
+
+		// при чтении вернётся распакованный слайс байт
+		body, err := io.ReadAll(gz)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		fmt.Fprintf(w, "Length: %d", len(body))
+
+	} else {
+		body, err := io.ReadAll(r.Body)
+		// обрабатываем ошибку
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		fmt.Fprintf(w, "Length: %d", len(body))
 	}
-	fmt.Fprintf(w, "Length: %d", len(body))
+
+}
+
+func main() {
+
 }
