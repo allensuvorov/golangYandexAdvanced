24877c66279020d3b83d4474aa5efb90f62b1fa5
Allen <allenvs@mail.ru>
2022-10-26 18:15:12 +0600

    generate rand []byte with len 512

--- sprint3/hash/task2/main.go
+++ sprint3/hash/task2/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"crypto/md5"
+	"crypto/rand"
 	"fmt"
 )
 
@@ -14,6 +15,13 @@ func main() {
 	)
 	// допишите код
 	// 1) генерация data длиной 512 байт
+	data = make([]byte, 512)
+	_, err := rand.Read(data) // записываем байты в массив data
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		return
+	}
+
 	// 2) вычисление hash1 с использованием md5.New
 	// 3) вычисление hash2 функцией md5.Sum
 
