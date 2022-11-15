223af5a29fd447be1c7072333f54a084e600ade9
Allen <allenvs@mail.ru>
2022-11-15 16:26:03 +0600

    Goroutines/Task1/main.go added

---
+++ sprint4/Goroutines/Task1/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+	n := 10
+	wg.Add(n)
+	for i := 0; i < n-1; i++ {
+		go func(v int) {
+			fmt.Println(v)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+}
